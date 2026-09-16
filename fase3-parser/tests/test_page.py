cf0d48f2015b74bbe89c86a89ca3a86640b94d3f
pedrsilva3515 <162255292+pedrsilva3515@users.noreply.github.com>
2026-09-16 19:07:56 -0300

    feat: decode lab fill colors

--- fase3-parser/tests/test_page.py
+++ fase3-parser/tests/test_page.py
@@ -161,6 +161,11 @@ class TestPreenchimentoETransparencia(unittest.TestCase):
         self.assertEqual(estilo.preenchimento.cor_primaria.modelo, "GRAY255")
         self.assertEqual(estilo.preenchimento.cor_primaria.componentes, (128,))
 
+    def test_preenchimento_lab(self):
+        estilo = self._estilo("caso_92_preenchimento_lab.cdr")
+        self.assertEqual(estilo.preenchimento.cor_primaria.modelo, "LAB")
+        self.assertEqual(estilo.preenchimento.cor_primaria.componentes, (50, 20, -30))
+
     def test_sobreimpressao_do_preenchimento(self):
         estilo = self._estilo("caso_70_preenchimento_sobreimpressao.cdr")
         self.assertTrue(estilo.preenchimento.sobreimpressao)
